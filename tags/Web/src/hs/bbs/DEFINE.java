b9c71f197d1b6ee44c548dce66e9dbe8ec445eb9
sdahhj <sdahhj@a3eeb3b4-cf4c-287c-947c-62481daca6af>
2013-08-06 07:04:46 +0000

    delete

--- tags/Web/src/hs/bbs/DEFINE.java
+++
@@ -1,8 +0,0 @@
-package hs.bbs;
-
-public final class DEFINE {
-	public static void printTAG(String ClassName, String msg) {
-		// TODO Auto-generated method stub
-		System.out.println(ClassName +"| "+msg);
-	}
-}
